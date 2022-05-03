1fc49d2b820636ac5f5d6e2496f781abce68b65d
steve <mbuguagichimu@gmail.com>
2022-05-03 21:59:06 +0300

    Dice Roller app

---
+++ app/src/androidTest/java/com/example/diceroller/ExampleInstrumentedTest.kt
@@ -0,0 +1,30 @@
+package com.example.diceroller
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import junit.framework.TestCase.assertTrue
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.diceroller", appContext.packageName)
+    }
+    @Test
+    fun generates_number() {
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
+    }
+
+}
